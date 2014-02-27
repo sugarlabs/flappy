47510ab1c60294a7cc120b295cfed27e73a20054
Alan Aguiar <alanjas@hotmail.com>
2014-02-27 20:33:12 -0200

    add copy notice

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+# Flappy
+# Copyright (C) 2014 Alan Aguiar
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Contact information:
+# Alan Aguiar alanjas@hotmail.com
 
 import pygame
 import sys
@@ -16,7 +35,6 @@ SKY = (113, 197, 207)
 GAME_SIZE = (684, 600)
 MIN_HEIGHT = 82 * 2 + 160
 DIST = 160
-
 
 
 
