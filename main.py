61f9e55c1c94a48a117f4577ecb3b2c2f7602a8d
Alan Aguiar <alanjas@hotmail.com>
2014-02-27 14:31:22 -0200

    basic pipe

--- main.py
+++ main.py
@@ -7,16 +7,40 @@ import pygame.sprite as sprite
 
 SKY = (113, 197, 207)
 GAME_SIZE = (800, 700)
+MIN_HEIGHT = 82 * 2 + 160
 
 class Floor():
 
     def __init__(self, large):
         self.p = large / 66
         self.large = self.p * 66
+        if (self.large % 5) == 0:
+            self.dx = 5
+        else:
+            l = self.large / 5
+            l = l + 1
+            self.dx = l * 5 - self.large
         self.image = pygame.surface.Surface((self.large, 16), 0)
         self.piece = pygame.image.load('floor.png')
         for i in range(self.p):
             self.image.blit(self.piece, (i * 66, 0))
+
+class Pipe():
+
+    def __init__(self, height):
+        if height < 82:
+            height = 82
+        h = height - 42
+        self.p = h / 40
+        self.height = self.p * 40 + 42
+        self.head = pygame.image.load('head_pipe.png')
+        self.body = pygame.image.load('pipe.png')
+        self.image = pygame.surface.Surface((91, self.height), 0)
+        self.image.fill((255, 255, 255))
+        self.image.set_colorkey((255, 255, 255))
+        self.image.blit(self.head, (0, 0))
+        for i in range(self.p):
+            self.image.blit(self.body, (4, 42 + i * 40))
 
 class Flappy():
 
@@ -26,8 +50,10 @@ class Flappy():
         self.floor = Floor(GAME_SIZE[0])
         self.floor_y = GAME_SIZE[1] - 50
         self.floor_x = 0
-        self.l = self.floor.large
+        self.l = self.floor.large - self.floor.dx
         self.floor_x1 = self.l
+        ########################################################################
+        self.pipe = Pipe(122)
 
     def main(self):
         self.clock = pygame.time.Clock()
@@ -39,16 +65,18 @@ class Flappy():
                     self.running = False
             self.screen.fill(SKY)
             self.screen.blit(self.build, (0, self.build_y))
-            self.floor_x -= 5
-            self.floor_x1 -= 5
+            self.screen.blit(self.pipe.image, (500, 200))
 
             self.screen.blit(self.floor.image, (self.floor_x, self.floor_y))
             self.screen.blit(self.floor.image, (self.floor_x1, self.floor_y))
+            self.floor_x = self.floor_x - 5
+            self.floor_x1 = self.floor_x1 - 5
+
             if self.floor_x < -self.l:
                 self.floor_x = self.l
             if self.floor_x1 < -self.l:
                 self.floor_x1 = self.l
-
+            
 
             pygame.display.flip()
             self.clock.tick(30)
