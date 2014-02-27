17cfdbde4c455fab230712bb258c4fcca2bb1c0b
Alan Aguiar <alanjas@hotmail.com>
2014-02-27 17:19:24 -0200

    bird up and restore to horizontal

--- bird.py
+++ bird.py
@@ -4,8 +4,8 @@
 import pygame
 import random
 
-bird1 = pygame.image.load('images/bird_h_alfa.png')
-
+bird_h = pygame.image.load('images/bird_h_alfa.png')
+bird_u = pygame.image.load('images/bird_u_alfa.png')
 
 class Bird(pygame.sprite.Sprite):
 
@@ -13,28 +13,36 @@ class Bird(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
 
         self.parent = parent
-        self.image = bird1
+        
         self.mPos = [200, y]
         self.mVel = 0
-        self.mAcc = 5
-        self.rect = self.image.get_rect()
-        self.rect.x = self.mPos[0]
-        self.rect.y = self.mPos[1]
+        self.mAcc = 5   
+        self.image = None
+        self.setImage(bird_h)
         self.count = 0
+
+    def setImage(self, img):
+        if not(self.image == img):
+            self.image = img
+            self.rect = self.image.get_rect()
+            self.rect.x = self.mPos[0]
+            self.rect.y = self.mPos[1]
 
     def setVel(self, vel):
         self.mVel = vel
         self.count = 20
+        self.setImage(bird_u)
 
     def update(self):
         self.count = self.count - 1
         if self.count < 0:
             self.count = 0
+            self.setImage(bird_h)
             self.mPos[1] = self.mPos[1] + self.mAcc
             self.rect.y = self.mPos[1]
         else:
             self.mPos[1] = self.mPos[1] - self.mVel
-
+            
         self.rect.y = self.mPos[1]
 
 
