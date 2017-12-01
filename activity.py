bf2a61fad279c9355f0f775a7173280bff617514
Santiago Acquarone <santiacquarone@gmail.com>
2017-12-01 20:17:53 -0300

    Remove unnecesary version requirements

--- activity.py
+++ activity.py
@@ -3,8 +3,6 @@
 
 import gi
 gi.require_version('Gtk', '3.0')
-gi.require_version('Rsvg', '2.0')
-gi.require_version('SugarExt', '1.0')
 from gi.repository import Gtk
 import sugargame
 import sugargame.canvas
