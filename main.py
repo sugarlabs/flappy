ea87bca937c3f80af34e7754b2129a3b5f1c9f3c
Adityakumar Sinha <adityakumar113141@gmail.com>
2021-02-07 22:45:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -36,8 +36,8 @@ from scores import EndScore
 from scores import Message
 from scores import CurrentScore
 
-#GAME_SIZE = (684, 600)
-GAME_SIZE = (1200, 700)
+GAME_SIZE = (684, 600)
+#GAME_SIZE = (1200, 700)
 
 FLOOR_Y = 50
 DIST = 160
